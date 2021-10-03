8aefc7b59107de0c8164a3a72c5acf3489d84e35
Stuart MacDonald <stuart.macd@gmail.com>
2021-10-03 16:00:47 +1300

    working

--- scripts.sikuli/scripts.py
+++ scripts.sikuli/scripts.py
@@ -4,9 +4,13 @@ def openBrowser():
     """
     Simple script to open the browser
     """
-    type(Key.CTRL+"t")
-    wait(2)
+    keyDown(Key.ALT+"d")
+    wait(0.5)
+    keyUp
 
+def fullScreen():
+    type(Key.F11)
+    wait(0.3)
 def clearBrowserCache():
     """
     For testing we are best to clear the cache before doing our first test
@@ -18,26 +22,32 @@ def hello():
     popup("Example scenario for Penny and Mick for using Sikulix")
 
 def openDevTools():
-    type(Key.CTRL + Key.SHIFT+"I")
-    wait(0.5)
+    keyDown(Key.CTRL + Key.SHIFT+"I")
+    keyUp()
+    
+    wait(1)
     
 def openArcGISDemo():
-    type(Key.CTRL+"t")
-    wait(1)
+#    keyDown(Key.ALT+"d")
+#    keyUp()
+#    wait(1)
     type("1633223686315.png","https://www.arcgis.com/home/webmap/viewer.html?webmap=286415a3edcd43f89fa266edc0c89b08"+Key.ENTER)
     wait(3)
     
 def newTab():
-       type(Key.CTRL,"t")
+       keyDown(Key.CTRL+"t")
        wait(0.5)
+       keyUp()
+       type(Key.BACKSPACE)
 
 def enterPhysicalAddress():
     type("1633224473540.png","1 The Terrace, Wellington" + Key.ENTER)
     wait(1)
 
 # hello()
-# newTab()
-# openDevTools()
+# fullScreen()
+openDevTools()
+newTab()
 openArcGISDemo()
 enterPhysicalAddress()
 
