98a92b0a0582397f5e6a9bbe0a5b857f6f0b6d6e
Stuart MacDonald <stuart.macd@gmail.com>
2021-10-03 17:53:16 +1300

    nice workflow forming

--- scripts.sikuli/scripts.py
+++ scripts.sikuli/scripts.py
@@ -1,12 +1,6 @@
 # Demo script to show how you can use 
 
-def openBrowser():
-    """
-    Simple script to open the browser
-    """
-    keyDown(Key.ALT+"d")
-    wait(0.5)
-    keyUp
+
 
 def fullScreen():
     type(Key.F11)
@@ -21,33 +15,70 @@ def clearBrowserCache():
 def hello():
     popup("Example scenario for Penny and Mick for using Sikulix")
 
+def newTab():
+       keyDown(Key.CTRL+"t")
+       keyUp()
+       wait(0.1)
+       type(Key.BACKSPACE)
+
 def openDevTools():
     keyDown(Key.CTRL + Key.SHIFT+"I")
+    keyUp()  
+    wait(0.5)
+
+def persistLogs():
+    click("1633236015299.png")
+    wait(0.1)
+    click("1633236718858.png")
+    
+    
+def startPerformanceAnalysis():
+    click("1633231374776.png")
+    
+def urlFocus():
+    keyDown(Key.ALT+"d")
+
     keyUp()
-    
-    wait(1)
-    
+    wait(0.2)    
+#    type(Key.BACKSPACE)
+#    wait(0.3)
+
 def openArcGISDemo():
 #    keyDown(Key.ALT+"d")
 #    keyUp()
-#    wait(1)
-    type("1633223686315.png","https://www.arcgis.com/home/webmap/viewer.html?webmap=286415a3edcd43f89fa266edc0c89b08"+Key.ENTER)
-    wait(3)
+#    wait(1)  
+    type("https://www.arcgis.com/home/webmap/viewer.html?webmap=286415a3edcd43f89fa266edc0c89b08"+Key.ENTER)
+    wait(8)
     
-def newTab():
-       keyDown(Key.CTRL+"t")
-       wait(0.5)
-       keyUp()
-       type(Key.BACKSPACE)
+
 
 def enterPhysicalAddress():
     type("1633224473540.png","1 The Terrace, Wellington" + Key.ENTER)
-    wait(1)
+    wait(8)
 
+def measure():
+    click("1633231670826.png")
+    wait(0.2)
+    click("1633231894570.png")
+    wait(0.2)
+    click("1633236202678.png")
+
+def area():
+    click("1633231741073.png")
 # hello()
 # fullScreen()
-openDevTools()
 newTab()
+openDevTools()
+persistLogs()
+urlFocus()
 openArcGISDemo()
 enterPhysicalAddress()
+measure()
+startPerformanceAnalysis()
+wait(5)
+
+# measure()
+
+# area()
+
 
